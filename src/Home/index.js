7a31172911003d91de6beb5e32403d836f82d5fe
manigoldTLC <gabrxavier353@gmail.com>
2023-01-05 23:30:26 -0300

    removendo pasta its me

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Container} from './styles';
 
 import Default from '../components/Default';
-import ItsMe from '../components/ItsMe';
 import ToDo from '../components/ToDo';
 
 const Home = () => {
